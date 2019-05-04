243f6c81acff4b9a5b7a03f4cf9fdabcabd97250
Andrea Valliere <amvalliere@gmail.com>
2019-05-04 01:37:58 -0700

    begin p5 background experimenting

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@ let canvas
 // Constants
 const Y_AXIS = 1;
 const X_AXIS = 2;
-let b1, b2, c1, c2;
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight)
@@ -11,59 +10,47 @@ function windowResized() {
 function setup() { 
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0)
-  canvas.style('z-index', '-1')
-  // createCanvas(windowWidth, windowHeight);
+  canvas.style('z-index', '-1') 
 
-    // Define colors
-    b1 = color(255);
-    b2 = color(0);
-    c1 = color(204, 102, 0);
-    c2 = color(0, 102, 153);
-  
-    noLoop();
+  colorArray = [color(105, 221, 255), color(150, 205, 255), color(216, 225, 255), color(219, 186, 221), color(190, 146, 162)]
+  darkColors = [color(62, 147, 173), color(103, 147, 191), color(183, 192, 229), color(192, 155, 196), color(165, 119, 136)]
+
+  noLoop();
 } 
+
+function setGradient(x, y, w, h, c1, c2) {
+  noFill();
+
+  for (let j = x; j <= x+w; j++) {
+    var inter2 = map(j, x, x+w, 0, 1);
+    var d = lerpColor(c1, c2, inter2);
+    stroke(d);
+    line(j, y, j, y+h);
+  }
+}
+
+function randomVal(axis) {
+  if (axis === 'X') {
+    return random(windowWidth)
+  }
+  if (axis === 'Y') {
+    return random(windowHeight)
+  }
+}
+
+function addObjects() {
+  for (var i = 0; i < 50; i++) {
+
+    noStroke();
+    fill(random(darkColors));
+    triangle(randomVal('X'), randomVal('Y'), randomVal('X'), randomVal('Y'), randomVal('X'), randomVal('Y'))
+    quad(randomVal('X'), randomVal('Y'), randomVal('X'), randomVal('Y'), randomVal('X'), randomVal('Y'), randomVal('X'), randomVal('Y'))
+    ellipse(randomVal('X'), randomVal('Y'), random(15), random(15));
+  }
+}
   
-  // function draw() { 
-  //   if (mouseIsPressed) {
-  //     fill(0);
-  //   } else {
-  //     fill(255);
-  //   }
-  //   ellipse(mouseX, mouseY, 80, 80);
-  // }
-
-
-
-
-
 function draw() {
-  background('#fae')
-//   // Background
-//   setGradient(0, 0, width / 2, height, b1, b2, X_AXIS);
-//   setGradient(width / 2, 0, width / 2, height, b2, b1, X_AXIS);
-//   // Foreground
-//   setGradient(50, 90, 540, 80, c1, c2, Y_AXIS);
-//   setGradient(50, 190, 540, 80, c2, c1, X_AXIS);
-// }
-
-// function setGradient(x, y, w, h, c1, c2, axis) {
-//   noFill();
-
-//   if (axis === Y_AXIS) {
-//     // Top to bottom gradient
-//     for (let i = y; i <= y + h; i++) {
-//       let inter = map(i, y, y + h, 0, 1);
-//       let c = lerpColor(c1, c2, inter);
-//       stroke(c);
-//       line(x, i, x + w, i);
-//     }
-//   } else if (axis === X_AXIS) {
-//     // Left to right gradient
-//     for (let i = x; i <= x + w; i++) {
-//       let inter = map(i, x, x + w, 0, 1);
-//       let c = lerpColor(c1, c2, inter);
-//       stroke(c);
-//       line(i, y, i, y + h);
-//     }
-//   }
+  setGradient(0, 0, windowWidth, windowHeight, random(colorArray), random(colorArray));
+  addObjects()
+  // background('#fae')
 }
